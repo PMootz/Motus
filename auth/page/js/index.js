290eb1d2ddbb815d1ad5695782c35180fd385111
PMootz <philmootz9@gmail.com>
2024-03-08 00:59:58 +0100

    Updating the visual

--- auth/page/js/index.js
+++ auth/page/js/index.js
@@ -4,20 +4,3 @@ $.get('/user', function(data){
         $('#user').text(data);
     }
 })
-
-//
-document.getElementById("connexion").addEventListener("click", function(event){
-    //cancel the defaut action of the button
-    event.preventDefault();
-    var id = $('#log').val();
-    var passW = $('#pass').val();
-    //check if all the field are completed
-    if (id.trim() === "" || passW.trim() === "") {
-        alert("Veuillez entrer des valeurs dans les 2 cases");
-        return; // Stop further execution
-    }
-    //check if the connection input are right or not
-    $.get('/login?user='+id +'&pass='+passW,function(data){
-        $('#reponse').html(data);
-    })
-})
